338ebdd9df06daf35c9d719832f7d98add405086
LUCAS GRAMKOW ALVES DE DEUS <lucas.deus@alunos.sc.senac.br>
2021-08-03 18:26:16 -0300

    novo projeto

---
+++ TemplateBranco/src/templatebranco/TemplateBranco.java
@@ -0,0 +1,40 @@
+// programa.teste.olamundo
+
+package templatebranco;
+
+/**
+ *
+ * @author lucas.deus
+ */
+public class TemplateBranco {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String strl = "ola mundo";
+        int a = 6;
+        int b = 7;
+        int c = a + b;
+         System.out.println(strl);
+         System.out.println("6+7 = " + c);
+        int d = 3;
+        int e = 4;
+        int f = 5;
+        int g = 2;    
+        int h = d + e;
+        int i = 6
+        int j = 8
+        System.out.println(strl);
+        System.out.println("3+4 = " + h);
+        int h = d + f;
+         System.out.println(strl);
+        System.out.println("3+5 = " + h);
+        int h = d + g;
+         System.out.println(strl);
+        System.out.println("3+2 = " + h);
+        int h = d + i
+        System.out.println(strl);
+         System.out.println("3+6 = " + h);
+         
+    }
